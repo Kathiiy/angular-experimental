2ed4cc7b56919593ccd413e1a3aee6b10dd35bbc
Kathiiy <katharinakomar@gmail.com>
2019-11-20 23:05:50 +0100

    install pug templating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ArticleComponent } from "./article/article.component";
+import { FooterComponent } from "./footer/footer.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "articles",
+    component: ArticleComponent
+  },
+  {
+    path: "footer",
+    component: FooterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
